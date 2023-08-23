136264d6ecf23bdfb576775aa68034e44306f190
elina <papadelina145@gmail.com>
2023-08-23 09:24:35 +0300

    Implement title

---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+import '../styles/Title.css';
+
+export default function Title() {
+    return (
+        <div className="title">
+            <h1><span className="first-part">Colour </span>
+             <span className="second-part">Memo</span></h1>
+        </div>
+    )
+}
